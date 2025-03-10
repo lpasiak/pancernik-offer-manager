379ed8b26d95736ce9e6619f62004f3f934a5aed
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-03-10 10:22:06 +0100

    Categories shoper function

--- connections/shoper/categories.py
+++ connections/shoper/categories.py
@@ -8,4 +8,38 @@ class ShoperAttributes:
         """Initialize a Shoper Client"""
         self.client = client
 
+    def get_all_categories(self):
+        """Get all categories from Shoper and return them as df or None and save to Excel."""
+        try:
+            print("Downloading all categories.")
+
+            categories = []
+            page = 1
+
+            while True:
+                params = {'limit': config.SHOPER_LIMIT, 'page': page}
+                response = self.client._handle_request('GET', f'{self.client.site_url}/webapi/rest/categories', params=params)
+                data = response.json()
+                number_of_pages = data['pages']
+
+                if response.status_code != 200:
+                    print(f'❌ API Error: {response.status_code}, {response.text}')
+                    return None
+
+                page_data = response.json().get('list', [])
+
+                if not page_data:
+                    break
+            
+                print(f'Page: {page}/{number_of_pages}')
+                categories.extend(page_data)
+                page += 1
+            
+            df = pd.DataFrame(categories)
+            df.to_excel(config.SHEETS_DIR / 'shoper_all_categories.xlsx', index=False)
+            return df
         
+        except Exception as e:
+            print(f'❌ Error fetching all categories: {str(e)}')
+            return None
+    
