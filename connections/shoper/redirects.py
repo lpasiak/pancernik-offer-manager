1b182d7c033d863c7d33828ff2b7029aadab03e8
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-04-28 12:48:45 +0200

    Code for redirects

---
+++ connections/shoper/redirects.py
@@ -0,0 +1,66 @@
+import config
+
+
+class ShoperRedirects:
+    def __init__(self, client):
+        """Initialize a Shoper Client"""
+        self.client = client
+
+    def create_redirect(self, redirect_data):
+        """Create a redirect for a product.
+        Args:
+            redirect_data (dict): Data about the discount:
+                redirected_url: string
+                target_url: string
+
+        Returns:
+            int|None: Redirect ID if successful, None if failed
+        """
+        try:
+
+            params = {
+                'route': redirect_data['redirected_url'],
+                'type': 0,
+                'target': redirect_data['target_url'],
+            }
+            
+            url = f'{config.SHOPER_SITE_URL}/webapi/rest/redirects'
+
+            response = self.client._handle_request('POST', url, json=params)
+            redirect_id = response.json()
+
+            if response.status_code == 200:
+                print(f'✅ Redirect {redirect_id} created')
+                return response.json()
+            else:
+                error_description = response.json()['error_description']
+                print(f'❌ API Error: {error_description}')
+                return {'success': False, 'error': error_description}
+            
+        except Exception as e:
+            print(f'❌ Request failed: {str(e)}')
+            return str(e)
+
+    def remove_redirect(self, identifier):
+        """Remove a redirect
+        Args:
+            identifier (int|str): Redirect id
+        Returns:
+            bool: True if successful, False if failed
+        """
+
+        try:
+            url = f'{config.SHOPER_SITE_URL}/webapi/rest/redirects/{identifier}'
+            response = self.client._handle_request('DELETE', url)
+
+            if response.status_code == 200:
+                print(f'✅ Special offer {identifier} removed successfully.')
+                return True
+            else:
+                error_description = response.json()['error_description']
+                print(f'❌ API Error: {error_description}')
+                return {'success': False, 'error': error_description}
+                    
+        except Exception as e:
+            print(f'❌ Request failed: {str(e)}')
+            return str(e)
