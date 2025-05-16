7ba177463ff3d3b66767861256602cc1fd74d032
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-05-16 10:41:08 +0200

    Add logger to EasyStorage products connection

--- connections/easystorage/products.py
+++ connections/easystorage/products.py
@@ -1,3 +1,4 @@
+from utils.logger import get_outlet_logger
 
 
 class EasyStorageProducts:
@@ -5,6 +6,8 @@ class EasyStorageProducts:
         """Initialize a Shoper Client"""
         self.client = client
         self.products_endpoint = 'https://easystorage.engine.pancernik.local/api/v1/products/easystorage/'
+        self.outlet_logger = get_outlet_logger().get_logger()
+
 
     def get_pancernik_products(self):
         
@@ -12,11 +15,14 @@ class EasyStorageProducts:
             params = {'account_id': self.client.pancernik_account_id}
 
             print('ℹ️  Downloading Pancernik products from EasyStorage...')
+            self.outlet_logger.info('ℹ️ Downloading Pancernik products from EasyStorage...')
+            
             response = self.client.session.request(method='GET', url=self.products_endpoint, params=params, verify=False)
             return response.json()
         
         except Exception as e:
             print(f"❌ Error downloading Pancernik products from EasyStorage: {e}")
+            self.outlet_logger.critical(f"❌ Error downloading Pancernik products from EasyStorage: {e}")
             return None
     
     def get_bizon_products(self):
@@ -24,10 +30,14 @@ class EasyStorageProducts:
         try:
             params = {'account_id': self.client.bizon_account_id}
 
-            print('ℹ️  Downloading Bizon products from EasyStorage...')
+            print('ℹ️ Downloading Bizon products from EasyStorage...')
+            self.outlet_logger.info('ℹ️ Downloading Bizon products from EasyStorage...')
+
             response = self.client.session.request(method='GET', url=self.products_endpoint, params=params, verify=False)
             return response.json()
         
         except Exception as e:
             print(f"❌ Error downloading Bizon products from EasyStorage: {e}")
+            self.outlet_logger.critical(f"❌ Error downloading Bizon products from EasyStorage: {e}")
+
             return None
