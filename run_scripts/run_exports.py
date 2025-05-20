54ea9c9a30ffa1d57d3f76f5740c476a1492651c
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-05-20 15:15:45 +0200

    Add absolute path to run_scripts in order to make them work

--- run_scripts/run_exports.py
+++ run_scripts/run_exports.py
@@ -1,3 +1,8 @@
+import os, sys
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 from orchestrators.export_orchestrator import run_massive_exporter
 
 if __name__ == "__main__":
--- run_scripts/run_promos.py
+++ run_scripts/run_promos.py
@@ -1,3 +1,8 @@
+import os, sys
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
 from orchestrators.promo_orchestrator import run_promo_manager
 
 if __name__ == "__main__":
