8018689bed3e41be7f9a9758ff91b5290e56a417
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-06-09 10:11:37 +0200

    Changes in google feed

--- google_feed_manager/google_feed_manager.py
+++ google_feed_manager/google_feed_manager.py
@@ -44,25 +44,67 @@ class GoogleCostOfGoodsSold:
         try:
             with open(filepath, 'r', encoding='utf-8') as file:
                 data = json.load(file)
+                if isinstance(data, dict):
+                    # If data is a dictionary, convert its values to a list
+                    data = list(data.values())
+                elif not isinstance(data, list):
+                    # If data is neither dict nor list, wrap it in a list
+                    data = [data]
                 return pd.DataFrame(data)
         except Exception as e:
             print(f'❌ Error in _convert_json_to_df: {e}')
 
-    def get_all_products_from_shoper(self):
-        df_products = self._convert_json_to_df(config.SHEETS_DIR / 'api-exports' / 'shoper-beautiful-products.json')
-        return df_products
+    def get_all_products_from_shoper_export(self):
+        product_df = self._convert_json_to_df(config.SHEETS_DIR / 'api-exports' / 'shoper-beautiful-products.json')
 
-    def import_bizon_prices_to_shoper(self):
+        product_mask = (
+            (~product_df['code'].str.contains('szablon', case=False, na=False)) &
+            (~product_df['code'].str.contains('out', case=False, na=False)) &
+            (product_df['cogs'] == '')
+        )
+
+        product_df['code'] = product_df['code'].astype(str)
+        product_df = product_df[product_mask]
+
+        return product_df
+
+    def import_bizon_prices_to_shoper(self, product_df):
         price_df = self.gsheets_worksheets.get_data('Bizon')
-        product_df = self.get_all_products_from_shoper()
 
-        # mask = (
-        #     (product_df['producer'] == 'Bizon') &
+        product_df = product_df[product_df['producer'] == 'Bizon']
 
-        # )
-        # product_df = product_df[mask]
+        price_mask = (
+            (price_df['EAN'].notna()) &
+            (price_df['EAN'] != '') &
+            (price_df['EAN'] != 0) &
+            (price_df['EAN'] != '0') &
+            (price_df['EAN'] != '#N/A')
+        )
+        price_df = price_df[price_mask]
+        price_df['EAN'] = price_df['EAN'].astype(str)
+        
+        bizon_product_df = pd.merge(
+            product_df,
+            price_df,
+            left_on='code',
+            right_on='EAN',
+            how='left'
+        )
 
-        print(price_df)
-        print(product_df)
+        bizon_product_df['COGS'] = bizon_product_df['Koszt produkcji netto'].astype(str).str.replace(',', '.') + ' PLN'
+        bizon_product_df = bizon_product_df[['product_id', 'code', 'COGS', 'Koszt produkcji netto']]
 
+        for index, row in tqdm(bizon_product_df.iterrows(), total=len(bizon_product_df), desc="Updating Bizon products", unit=" product"):
+            self.shoper_products.update_product_by_code(row['product_id'], 
+                                                        attributes={config.COST_OF_GOODS_SOLD['id']: row['COGS']},
+                                                        stock={'price_buying': row['Koszt produkcji netto']})
+            
+    def import_bewood_dropshipping_prices_to_shoper(self, product_df):
+        price_df = self.gsheets_worksheets.get_data('Bewood', include_row_numbers=False)
 
+        product_mask = (
+            (product_df['producer'] == 'Bewood') &
+            (product_df['gauge'].str.contains('Wydłużony czas realizacji'))
+        )
+
+        product_df = product_df[product_mask]
